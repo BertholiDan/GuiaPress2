f5712fc420a55d856615159815306c463be1bfe5
Danilo <danilobertholi@gmail.com>
2024-01-05 14:25:26 -0300

    aula 116 - Finalizando paginação

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("guiapress2", "root", "dataatecnologia", {
   host: "localhost",
   dialect: "mysql",
+  logging: false,
   timezone: "-03:00",
 });
 
